fc49db71fc28fd88f35ff22d088b9d6ed619a458
JerryOuyang <jerry830720@gmail.com>
2025-11-24 00:47:53 +0800

    Final Fix:corrcet App.jsx import path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // 引用了您剛才編輯的 App.jsx
+import App from './App.jsx'; // *** 將這一行改為 .jsx ***
 
 ReactDOM.render(
   <React.StrictMode>
