4f885db7592e3b3d2bd00a29ad53f82bf6ee2ed8
JerryOuyang <jerry830720@gmail.com>
2025-11-24 00:52:19 +0800

    Final Fix:Standardize component filename to app.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx'; // *** 將這一行改為 .jsx ***
+import App from './app.js'; 
 
 ReactDOM.render(
   <React.StrictMode>
